0e9d0b4b843f2f9d5a15408a778c198d8d92bbd4
Bradley Nichol <bradleynichol@live.co.uk>
2021-09-22 22:53:12 +0100

    Changing auth window

--- src/app/insta-auth-button/insta-auth-button.component.ts
+++ src/app/insta-auth-button/insta-auth-button.component.ts
@@ -25,6 +25,8 @@ export class InstaAuthButtonComponent implements OnInit {
           `client_id=${this.CLIENT_ID}`+
           `&redirect_uri=${this.REDIRECT_URL}`+
           '&scope=user_profile,user_media&response_type=code',
+          "child",
+          "width=430,height=400,left=500,top=100"
         );
   }
 
